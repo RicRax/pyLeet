587ed4f96605b4822c0cf560af3cd7c13c041049
riccardo <ricraz303@gmail.com>
2023-11-14 17:00:15 +0100

    get question list command

---
+++ src/conf/graphql.py
@@ -0,0 +1,55 @@
+from . import config
+
+leetCodeUrl = "https://leetcode.com/graphql"
+
+randomQuestionQuery = """query questionContent($titleSlug: String!) {
+  question(titleSlug: $titleSlug) {
+    content
+    titleSlug
+  }
+}"""
+
+
+cookies = {"LEETCODE_SESSION": config.leetCodeSession, "csrftoken": config.csrfToken}
+
+headers = {
+    "Content-Type": "application/json",
+    "x-csrftoken": config.csrfToken,
+    "Origin": "https://leetcode.com",
+    "Referer": "https://leetcode.com",
+    "Connection": "keep-alive",
+}
+
+
+problemSetQuery = """
+query problemsetQuestionList($categorySlug: String, $limit: Int, $skip: Int, $filters: QuestionListFilterInput) {
+    problemsetQuestionList: questionList(
+        categorySlug: $categorySlug
+        limit: $limit
+        skip: $skip
+        filters: $filters
+    ) {
+        total: totalNum
+        questions: data {
+            acRate
+            difficulty
+            freqBar
+            frontendQuestionId: questionFrontendId
+            isFavor
+            paidOnly: isPaidOnly
+            status
+            title
+            titleSlug
+            topicTags {
+                name
+                id
+                slug
+            }
+            hasSolution
+            hasVideoSolution
+        }
+    }
+}
+
+
+"""
---
+++ src/pyLeet.py
@@ -0,0 +1,61 @@
+import requests
+import json
+import click
+
+from conf import graphql
+
+
+def getQueryQuestionContent(titleSlug):
+    questionQuery = """query questionContent($titleSlug: String!) {
+      question(titleSlug: $titleSlug) {
+        content
+        titleSlug
+      }
+    }"""
+    variables = {"titleSlug": titleSlug}
+    return {"query": questionQuery, "variables": variables}
+
+
+def getFirst10Problems(limit):
+    variables = {"categorySlug": "", "skip": 0, "limit": limit, "filters": {}}
+    return {"query": graphql.problemSetQuery, "variables": variables}
+
+
+@click.command()
+@click.argument("limit", type=int, required=1)
+def getQuestionList(limit):
+    response = requests.post(
+        graphql.leetCodeUrl,
+        json=getFirst10Problems(limit),
+        cookies=graphql.cookies,
+        headers=graphql.headers,
+    )
+    python_object = json.loads(response.text)
+    for question in python_object["data"]["problemsetQuestionList"]["questions"]:
+        click.echo(question["title"])
+
+
+@click.command()
+@click.argument("titleslug", type=str, required=1)
+def getQuestion(titleslug):
+    response = requests.post(
+        graphql.leetCodeUrl,
+        json=getQueryQuestionContent(titleslug),
+        cookies=graphql.cookies,
+        headers=graphql.headers,
+    )
+    click.echo(response.text)
+
+
+@click.group()
+def cli():
+    """A Click Program to fetch LeetCode problems"""
+    pass
+
+
+cli.add_command(getQuestion)
+cli.add_command(getQuestionList)
+
+
+if __name__ == "__main__":
+    cli()
